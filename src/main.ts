be1bc6c87c3cc6844b8d6bffcde547f300e30514
Umaru <573112924@qq.com>
2023-08-09 15:53:20 +0800

    动态路由,pinia持久化,登录

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router/index'
+import router from '@/router/index'
+// 引入 createPinia 函数
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 import axios from '@/request/request'
 // 全局引入mock,仅在开发环境使用，打包时注释
 import '@/mock/index.js'
-
-const app = createApp(App).use(router)
+// 创建 pinia 实例（根 store）
+const pinia = createPinia()
+pinia.use(piniaPersist)
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
 app.mount('#app')
 //配置axios的全局引用
 app.config.globalProperties.$axios = axios
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,22 +1,87 @@
 <script setup lang="ts">
-import { onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
 // import { getUserInfo } from '@/request/api'
 import { getUserInfo } from '@/mock/api'
+import { useUserStore } from '@/stores/userInfo.js'
 
-onBeforeMount(() => {
-  //调用方法
-  getUserInfo()
-    .then(function (res) {
-      console.log(res.data)
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
+// onBeforeMount(() => {
+// })
+const router = useRouter()
+const userStore = useUserStore()
+
+interface RuleForm {
+  username: string
+  password: string
+}
+const ruleFormRef = ref<FormInstance>()
+const formLabelAlign = reactive<RuleForm>({
+  username: '',
+  password: '',
 })
-</script>
+const rules = reactive<FormRules<RuleForm>>({
+  username: [
+    { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+  ],
+})
 
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      //调用方法
+      getUserInfo()
+        .then(function (res) {
+          console.log('>>>>', res.data)
+          let { code, data } = res.data
+          if (code == 200) {
+            // 存储
+            userStore.registerUser(data)
+            // 跳转首页
+            router.push({
+              path: '/home',
+            })
+          }
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+console.log('444', userStore)
+</script>
 <template>
-  <div>login</div>
+  <div class="ghghgh">login</div>
+  <el-form label-position="right" label-width="100px" ref="ruleFormRef" :model="formLabelAlign" :rules="rules" style="max-width: 460px">
+    <el-form-item label="用户名" prop="username">
+      <el-input v-model="formLabelAlign.username" placeholder="请输入用户名" />
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="formLabelAlign.password" placeholder="请输入密码" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">登录</el-button>
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
+  <!-- <div>{{ userStore.username }}</div> -->
 </template>
 
-<style scoped></style>
+<style scoped>
+@import './index.css';
+</style>
