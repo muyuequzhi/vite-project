fcdefb7f80ee339e785bcc30998fc8993999ab21
Umaru <573112924@qq.com>
2024-03-15 11:26:55 +0800

    nothing

--- src/utils/function.js
+++ src/utils/function.js
@@ -1,3 +1,11 @@
 export const isArray = function (o) {
   return Object.prototype.toString.call(o) == '[object Array]'
 }
+// 格式化银行卡号
+export const formatCardNumber = function (number) {
+  number = number.toString() || ''
+  // 18位本行借记卡
+  if (number.startsWith('6223') && number.length === 18) {
+    number = number.slice(4)
+  }
+}
