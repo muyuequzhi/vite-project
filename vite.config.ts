6fc4c0c61d0e86b5be9d8c7f925dc2f1392eba99
Umaru <573112924@qq.com>
2024-03-18 16:53:51 +0800

    更新vite-plugin-mock使用

--- vite.config.ts
+++ vite.config.ts
@@ -7,49 +7,53 @@ import path from 'path'
 import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ command }) => {
+  // 根据项目配置。可以配置在.env文件
+  const prodMock = true
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
         @use "@/style/element/index.scss" as *;
         @use "@/style/common/var.scss" as *;
         `,
+        },
       },
     },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
-    }),
-    viteMockServe({
-      // default
-      mockPath: './src/mock/',
-      localEnabled: true,
-      prodEnabled: false,
-      // 监视文件更改
-      watchFiles: true,
-      // 动态控制生产环境是否开启 mock 且在没有开启的时候 mock.js 不会被打包
-      // 该代码会被注入到injectFile对应的文件的底部。默认injectFile为main.{ts,js}
-      injectCode: `
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+      }),
+      viteMockServe({
+        // default
+        mockPath: './src/mock/',
+        localEnabled: command === 'serve',
+        prodEnabled: command !== 'serve' && prodMock,
+        // 监视文件更改
+        watchFiles: true,
+        // 动态控制生产环境是否开启 mock 且在没有开启的时候 mock.js 不会被打包
+        // 该代码会被注入到injectFile对应的文件的底部。默认injectFile为main.{ts,js}
+        injectCode: `
                     import { setupProdMockServer } from './mock/mockProdServer';
                     setupProdMockServer();
                 `,
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/configuration-options/
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        // https://rollupjs.org/configuration-options/
+      },
+      sourcemap: 'hidden',
     },
-    sourcemap: 'hidden',
-  },
+  }
 })
