9622a285539657615fa3714f662e9b2ec09a3566
tangshiyang <1269382982@qq.com>
2024-03-22 11:15:48 +0800

    是

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,7 @@ module.exports = {
     'vue/multi-word-component-names': 0,
     'no-unused-vars': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': ['off'],
     '@typescript-eslint/no-this-alias': [
       'error',
       {
